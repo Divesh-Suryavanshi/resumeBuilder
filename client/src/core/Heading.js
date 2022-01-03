7e5e4ca40074e923ee5b60f10260be5544e2f001
Divesh <Diveshsisodiads@gmail.com>
2022-01-03 16:00:09 +0530

    modified file structure a little bit

--- client/src/core/Heading.js
+++ client/src/core/Heading.js
@@ -1,1 +1,10 @@
 import { useState } from "react";
+
+const Heading = (prop) => {
+  const []
+  return (
+    <div className="heading">
+      <h1>Whats the best way for employer to contact you</h1>
+    </div>
+  );
+};
